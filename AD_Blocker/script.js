ec9cbfadff97855f338ff253aaf400ff3ad76bef
Y Eswar Aditya <yeswaraditya3006@gmail.com>
2024-05-12 17:02:46 +0530

    update js

--- AD_Blocker/script.js
+++ AD_Blocker/script.js
@@ -1,42 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var adBlockerCheckbox = document.getElementById('adBlockerCheckbox');
+// document.addEventListener('DOMContentLoaded', function () {
+//     var adBlockerCheckbox = document.getElementById('adBlockerCheckbox');
 
-    adBlockerCheckbox.addEventListener('change', function () {
-        // Toggle ad blocking feature based on checkbox state
-        if (adBlockerCheckbox.checked) {
-            // Enable ad blocking
-            // Apply rules from JSON when the button is checked
-            applyRules();
-        } else {
-            // Disable ad blocking
-            // Reset ad blocking rules when the button is unchecked
-            resetRules();
-        }
-    });
-});
+//     // Function to toggle ad blocking when checkbox is clicked
+//     function toggleAdBlocking() {
+//         if (adBlockerCheckbox.checked) {
+//             // Enable ad blocking
+//             applyRules();
+//         } else {
+//             // Disable ad blocking
+//             resetRules();
+//         }
+//     }
 
-function applyRules() {
-    // Read the JSON rules file asynchronously
-    fetch('rules.json')
-        .then(response => response.json())
-        .then(rules => {
-            // Apply each rule
-            rules.forEach(rule => {
-                chrome.declarativeNetRequest.updateDynamicRules({
-                    removeRuleIds: [rule.id],
-                    addRules: [rule]
-                });
-            });
-        })
-        .catch(error => {
-            console.error('Error applying rules:', error);
-        });
-}
+//     // Apply ad blocking rules when checkbox is clicked
+//     adBlockerCheckbox.addEventListener('change', toggleAdBlocking);
+// });
 
-function resetRules() {
-    // Reset rules to empty array to disable ad blocking
-    chrome.declarativeNetRequest.updateDynamicRules({
-        removeRuleIds: [],
-        addRules: []
-    });
-}
+// function applyRules() {
+//     fetch('rules.json')
+//         .then(response => response.json())
+//         .then(rules => {
+//             chrome.declarativeNetRequest.updateDynamicRules({
+//                 removeRuleIds: [],
+//                 addRules: rules
+//             });
+//         })
+//         .catch(error => {
+//             console.error('Error applying rules:', error);
+//         });
+// }
+
+// function resetRules() {
+//     chrome.declarativeNetRequest.updateDynamicRules({
+//         removeRuleIds: [],
+//         addRules: []
+//     });
+// }
